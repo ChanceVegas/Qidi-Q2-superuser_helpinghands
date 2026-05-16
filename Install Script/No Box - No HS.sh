84cb01c1f283ff68e359003cda8cefae954651ac
Camden-Winder <camdenwinder@gmail.com>
2026-05-16 15:15:03 -0700

    Update No Box - No HS.sh

--- Install Script/No Box - No HS.sh
+++ Install Script/No Box - No HS.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
 echo "gcode_macro.cfg is being changed"
 
-curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/gcode_macro(no.box.no.hs).cfg -o /home/mks/printer_data/config/gcode_macro.cfg
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/gcode_macro%28no.box.no.hs%29.cfg \
+  -o /home/mks/printer_data/config/gcode_macro.cfg
 
 echo "KAMP settings are being adjusted"
 
-curl -sSL https://github.com/Camden-Winder/Qidi-Q2-superuser/blob/main/Install%20Script/KAMP_settings.cfg \ -o /home/mks/printer_data/config/KAMP_Settings.cfg
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/KAMP_settings.cfg \
+  -o /home/mks/printer_data/config/KAMP_Settings.cfg
 
 echo "congrats, everything is now installed"
