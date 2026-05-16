080ccf453ae7d4aaff776c6ce4596632c985c595
Camden-Winder <camdenwinder@gmail.com>
2026-05-16 14:32:49 -0700

    Update BB & HS.sh

--- Install Script/BB & HS.sh
+++ Install Script/BB & HS.sh
@@ -12,19 +12,19 @@ curl -sSL https://raw.githubusercontent.com/prestonbrown/helixscreen/main/script
 echo "Config changes are now being made"
 
 echo "gcode_macro.cfg is being changed"
-curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/main/Install%20Script/gcode_macro.cfg \
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/gcode_macro.cfg \
   -o /home/mks/printer_data/config/gcode_macro.cfg
 
 echo "printer.cfg is being changed"
-curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/main/Install%20Script/printer.cfg \
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/printer.cfg \
   -o /home/mks/printer_data/config/printer.cfg
 
 echo "KAMP settings are being adjusted"
-curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/main/Install%20Script/KAMP_settings.cfg \
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/KAMP_settings.cfg \
   -o /home/mks/printer_data/config/KAMP_Settings.cfg
 
 echo "helixscreen settings are being adjusted"
-curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/main/Install%20Script/helixscreen_settings.json \
+curl -sSL https://raw.githubusercontent.com/Camden-Winder/Qidi-Q2-superuser/refs/heads/main/Install%20Script/helixscreen_settings.json \
   -o /home/mks/helixscreen/config/settings.json
 
-echo "Congrats, everything should be installed"
+echo "Congrats, everything is now installed"
